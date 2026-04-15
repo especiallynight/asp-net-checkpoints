0af886b10570327048394fc399882d24846f7253
alina <degtyareva_18@mail.ru>
2026-04-15 23:23:42 +0300

    add checkpoint 18

---
+++ Checkpoint_18/Checkpoint_18/wwwroot/js/charts.js
@@ -0,0 +1,27 @@
+﻿window.drawChart = (labels, data) => {
+    const ctx = document.getElementById('salesChart');
+    if (!ctx) return;
+
+    let chartStatus = Chart.getChart(ctx);
+    if (chartStatus) {
+        chartStatus.destroy();
+    }
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Продажи',
+                data: data,
+                backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+    });
+};
